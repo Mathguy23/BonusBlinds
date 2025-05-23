4117d003316b32cf07058bb360d775f41643bab0
Mathguy23 <165744246+Mathguy23@users.noreply.github.com>
2025-05-22 21:26:35 -0400

    grim compat

--- BonusBlinds.lua
+++ BonusBlinds.lua
@@ -4,7 +4,7 @@
 --- PREFIX: bb
 --- MOD_AUTHOR: [mathguy]
 --- MOD_DESCRIPTION: Bonus Blinds
---- VERSION: 1.5.9
+--- VERSION: 1.5.9b
 ----------------------------------------------
 ------------MOD CODE -------------------------
 
